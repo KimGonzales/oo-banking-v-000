91ff0f8b0e7ee360fa8e1c199fe81e21c2819ab0
Kim Gonzales <kimgo589@gmail.com>
2018-02-09 05:39:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     #transfer cannot happen if
     # 1. it already happened (status is complete)
     # 2. the sender does not have a valid account
-    if @sender.valid? && @status == "pending"
+    if self.valid?==true && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
